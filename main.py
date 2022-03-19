1f9ffdcf0907a4ee88d08f0e57f67ac10ad122db
WhiteCrosses <99359429+WhiteCrosses@users.noreply.github.com>
2022-03-19 21:50:47 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -9,35 +9,49 @@ translator = Translator()
 
 global file
 global re_en
-
+global whatpage
 
 def filename():
-    file = input("Nazwa pliku: ")
+    file = input("File name: ")
     return file
+
+def numpages():
+    global whatpage
+    running = True
+    while running:
+        whatpage = input("Page: ")
+        if int(whatpage) > pdfReader.numPages or int(whatpage) < 0 :
+            print("Wrong page number!")
+        else:
+            running = False
+    return whatpage
 
 def metadata():
     global file
     global pdfReader
 
-    print(f"This PDF has {pdfReader.numPages} pages.\n\n")
+    print(f"This PDF has {pdfReader.numPages} pages.")
 
 def text():
     global en_
     global file
+    global whatpage
 
-    PageObject = pdfReader.getPage(2)
+    PageObject = pdfReader.getPage(int(whatpage))
     TextObject = str(PageObject.extractText())
 
 
-    en_en = re.sub('[\r\n?|\n][\r\n?|\n]', 'JP2GMD', TextObject,) #enter + enter
-    en_my = re.sub('-'+"(\r\n?|\n)", '', en_en) #myślnik + enter
-    en_ = re.sub("(\r\n?|\n)", ' ', en_my) #enter
-    re_en = re.sub('JP2GMD', ' \n', en_)
-    re_en = str(re_en)
+    #en_en = re.sub('[\r\n?|\n][\r\n?|\n]', 'JP2GMD', TextObject,) #enter + enter
+    format = re.sub('-'+"(\r\n?|\n)", '', TextObject) #myślnik + enter
+    format = re.sub("(\r\n?|\n)", ' ', format) #enter
+    #re_en = re.sub('JP2GMD', '\n', en_)
+    #re_en = re.sub('  ', '\n', re_en)
+    #format = format.replace(' \t', ' \n')
+    #format = re.sub([ \t], '\n', format)
 
     open('test.txt', 'w').close()
     file1=open('test.txt',"a")
-    file1.writelines(en_)
+    file1.writelines(format)
     pdfFileObject.close()
 
 def translate():
@@ -60,6 +74,7 @@ pdfFileObject = open('Fake News.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObject)
 
 metadata()
+numpages()
 text()
 translate()
 

b615e479a8428bbf80494461f6c6f52d1d284919
WhiteCrosses <99359429+WhiteCrosses@users.noreply.github.com>
2022-03-19 21:51:48 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -9,49 +9,35 @@ translator = Translator()
 
 global file
 global re_en
-global whatpage
+file = 0
 
-def filename():
-    file = input("File name: ")
-    return file
-
-def numpages():
-    global whatpage
-    running = True
-    while running:
-        whatpage = input("Page: ")
-        if int(whatpage) > pdfReader.numPages or int(whatpage) < 0 :
-            print("Wrong page number!")
-        else:
-            running = False
-    return whatpage
+#def filename():
+    #file = input("Nazwa pliku: ")
+    #return file
 
 def metadata():
     global file
     global pdfReader
 
-    print(f"This PDF has {pdfReader.numPages} pages.")
+    print(f"This PDF has {pdfReader.numPages} pages.\n\n")
 
 def text():
     global en_
     global file
-    global whatpage
 
-    PageObject = pdfReader.getPage(int(whatpage))
+    PageObject = pdfReader.getPage(2)
     TextObject = str(PageObject.extractText())
 
 
-    #en_en = re.sub('[\r\n?|\n][\r\n?|\n]', 'JP2GMD', TextObject,) #enter + enter
-    format = re.sub('-'+"(\r\n?|\n)", '', TextObject) #myślnik + enter
-    format = re.sub("(\r\n?|\n)", ' ', format) #enter
-    #re_en = re.sub('JP2GMD', '\n', en_)
-    #re_en = re.sub('  ', '\n', re_en)
-    #format = format.replace(' \t', ' \n')
-    #format = re.sub([ \t], '\n', format)
+    #edit = re.sub('[\r\n?|\n][\r\n?|\n]', 'JP2GMD', TextObject,) #enter + enter
+    edit = re.sub('-'+"(\r\n?|\n)", '', TextObject) #myślnik + enter
+    edit = re.sub("(\r\n?|\n)", ' ', edit) #enter
+    edit = re.sub('JP2GMD', ' \n', edit)
+    edit = str(edit)
 
     open('test.txt', 'w').close()
     file1=open('test.txt',"a")
-    file1.writelines(format)
+    file1.writelines(edit)
     pdfFileObject.close()
 
 def translate():
@@ -67,14 +53,10 @@ def translate():
     file1.writelines(str(result))
     pdfFileObject.close()
 
-
-filename()
-
-pdfFileObject = open('Fake News.pdf', 'rb')
+pdfFileObject = open(input("Nazwa pliku: "), 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObject)
 
 metadata()
-numpages()
 text()
 translate()
 
